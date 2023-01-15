54600c1e2503cb5a849b2758eef74c8cc9e63116
Carson Andorf <andorfc@iastate.edu>
2023-01-14 21:46:26 -0600

    Update s10_annotation.sh

--- s10_annotation.sh
+++ s10_annotation.sh
@@ -10,8 +10,10 @@ SPECIES2=arabidopsis
 BASE_DIR=/90daydata/maizegdb/carson/alphafold_test/FASSO/
 ########################
 PDB_DIR=${BASE_DIR}alignments/fasta_files/${SPECIES1}PDB_list.txt
-
 php ${BASE_DIR}php/make_pairwise_annotations.php ${SPECIES1} ${SPECIES2} ${PDB_DIR} ${BASE_DIR}
+
+PDB_DIR=${BASE_DIR}alignments/fasta_files/${SPECIES2}PDB_list.txt
+php ${BASE_DIR}php/make_pairwise_annotations.php ${SPECIES2} ${SPECIES1} ${PDB_DIR} ${BASE_DIR}
 
 date                          #optional, prints out timestamp when the job ends
 #End of file
